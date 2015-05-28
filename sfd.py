5ee5bbdbbb136b6338e7a0b501c659a5377d5911
Steven Brus <sbrus@nd.edu>
2015-05-28 15:29:48 -0400

    Updates to sfd.py
     - Only get the content of the user.comment namespace.  Dropbox also
       adds data to a different extended attribute namespace and these should
       be ignored in the output.
    
     - Clean up some comments

--- sfd.py
+++ sfd.py
@@ -2,7 +2,6 @@
 
 import subprocess
 import sys
-#import xattr
 import os
 import re
 
@@ -29,7 +28,7 @@ if not os.path.exists(full_name):
   raise SystemExit(0)
 
 # check if owner has write permissions
-#   - This may need to be considered more carefully if used when different owners/groups are involved
+#   - This will need to be considered more carefully if used when different owners/groups are involved
 cmd = "ls -ld " + full_name  
 ls_output = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,executable='/bin/bash').communicate()[0]
 
@@ -52,17 +51,19 @@ existing_attr = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,executa
 # open dummy file for extended attribute input (overwrite if already exists)
 f = open(".descrip","w")
 
-# write any existing obnect extended attibutes to file
+# write any existing object extended attibutes to file
 if existing_attr != "":
   file_info = filter(None,existing_attr.split("\n"))  #split output lines and get rid of empty strings
   for i in range(1,len(file_info)):  # loop through list of attribute values (begins on second line)
-                                     # the loop probably isn't necessary because this program only uses the user.comment attribute
-    ind = file_info[i].find("=")                            # find where comment begins
-    descrip = file_info[i].replace(file_info[i][:ind+1],"") # remove the name of the extended attrubute
-    comment = descrip.replace('"','').split(r"\012")        # split lines and remove quotes
+                                     # just in case other attrubutes are set, this program only uses the user.comment attribute
+    ind = file_info[i].find("user.comment=")                                 
+    if ind >= 0:                                     
+      ind =ind+len("user.comment=")                           # find where comment begins
+      descrip = file_info[i].replace(file_info[i][:ind+1],"") # remove the name of the extended attrubute
+      comment = descrip.replace('"','').split(r"\012")        # split lines and remove quotes
  
-    for line in comment:    # write to file
-      f.write("%s\n" % line)
+      for line in comment:    # write to file
+        f.write("%s\n" % line)
       
 f.write("\n")
 f.write("\n")
@@ -78,7 +79,7 @@ f = open(".descrip")
 cmnt = f.read().strip()
 f.close()
 
-
+# Process input comments
 cmnt_sp = cmnt.split("\n")
 cmnt_ls = []
 for line in cmnt_sp:
@@ -90,9 +91,8 @@ if cmnt == "":
   print "sfd: comment is empty, exiting..."
   raise SystemExit(0)
 
-# Add the inputed comment as an extended attribute
-#xattr.setxattr(file_name, "user.comment", cmnt)
 
+# Add the inputed comment as an extended attribute
 cmd = 'setfattr -n user.comment -v "' + cmnt + '" ' + full_name
 subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,executable='/bin/bash').communicate()[0]
 

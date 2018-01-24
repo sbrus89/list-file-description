53fdf20fe4eecf8c49a29cd4a460444d1af73566
Steven Brus <sbrus@nd.edu>
2018-01-24 11:18:49 -0500

    Updates to lfd.py and sfd.py
     - Add option to use hidden files instead of extended
       attributes to set and list the description information

--- lfd.py
+++ lfd.py
@@ -5,6 +5,9 @@ import os
 import pprint
 import re
 
+#method = "fattr"
+method = "file"
+
 # ansi codes for color terminal output
 MAGENTA = '\033[35m'
 WHTIE = '\033[37m'
@@ -17,48 +20,94 @@ UNDERLINE = '\033[4m'
 ENDC = '\033[0m'
 BOLD = "\033[1m"
 
+# get present directory
+pwd = os.getcwd()
+
 # object to remove ansi color codes from ls ouptut
 ansi_escape = re.compile(r'\x1b[^m]*m')
 
 # get the output of ls
-cmd = "ls -lhrt --color"
+cmd = "ls -lhrt --color " + pwd
 ls_output = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,executable='/bin/bash').communicate()[0]
 ls_sp = ls_output.splitlines()
 
-# get the extended attributes of all the files/directories in pwd
-pwd = os.getcwd()
-cmd = "getfattr -d --absolute-names " + pwd + "/*"
-getfattr_output = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,executable='/bin/bash').communicate()[0]
-getfattr_sp = filter(None,getfattr_output.split("# file:")) # split the output for each file
-
-files = {}
-for i in range(0,len(getfattr_sp)): # loop though all files with extended attributes
-  file_info = filter(None,getfattr_sp[i].split("\n")) # split the file information
-  full_name = file_info[0]                            # first line is: # file: /path/file
-  name = full_name.split("/")[-1]                     # find the name of the file at the end of the path
-  files[name] = []
+if method == "fattr":
   
-  for i in range(1,len(file_info)):  # loop through list of attribute values (begins on second line)
-                                     # Just in case other attributes are set, sfd.py only uses the user.comment attribute
-    ind = file_info[i].find("user.comment=")                                      
-    if ind >= 0:
-      ind = ind+len("user.comment=")                          # find where comment begins: "user.comment="comments"
-      descrip = file_info[i].replace(file_info[i][:ind+1],"") # remove the name of the extended attribute
-      comment = descrip.replace('"','').split(r"\012")        # split lines and remove quotes
+  # get the extended attributes of all the files/directories in pwd
+  cmd = "getfattr -d --absolute-names " + pwd + "/*"
+  getfattr_output = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,executable='/bin/bash').communicate()[0]
+  getfattr_sp = filter(None,getfattr_output.split("# file:")) # split the output for each file
+
+  files = {}
+  for i in range(0,len(getfattr_sp)): # loop though all files with extended attributes
+    file_info = filter(None,getfattr_sp[i].split("\n")) # split the file information
+    full_name = file_info[0]                            # first line is: # file: /path/file
+    name = full_name.split("/")[-1]                     # find the name of the file at the end of the path
+    files[name] = []
+  
+    for i in range(1,len(file_info)):  # loop through list of attribute values (begins on second line)
+                                       # Just in case other attributes are set, sfd.py only uses the user.comment attribute
+      ind = file_info[i].find("user.comment=")                                      
+      if ind >= 0:
+        ind = ind+len("user.comment=")                          # find where comment begins: "user.comment="comments"
+        descrip = file_info[i].replace(file_info[i][:ind+1],"") # remove the name of the extended attribute
+        comment = descrip.replace('"','').split(r"\012")        # split lines and remove quotes
+        while comment[-1] == "":
+          comment.pop()
+        if comment[0] == "":
+          comment.pop(0)
+        files[name].append(comment)
+        
+elif method == "file":
+  
+  files = {}
+  for line in ls_sp[1:]:                              # loop through lines of ls output
+    line_sp = line.split()
+    
+    # build name of description file
+    if line[0] == "l":
+      name = ansi_escape.sub("",line_sp[-3])      
+      full_name = ansi_escape.sub("",line_sp[-1])
+      target_name = full_name.split("/")[-1]
+    else:  
+      name = ansi_escape.sub("",line_sp[-1])             
+      full_name = pwd + "/" + name      
+       
+    if os.path.isfile(full_name):
+      extension = "-file.descrip"
+    elif os.path.isdir(full_name):
+      extension = "-dir.descrip"   
+      
+    if line[0] == "l":
+      fname = full_name + "/." + target_name + extension
+      print fname
+      print name
+    else:  
+      fname = pwd + "/." + name + extension     
+    
+    # get comments for files with descriptions
+    files[name] = []    
+    if os.path.exists(fname):    
+      f = open(fname)
+      comment = f.read().splitlines()
       while comment[-1] == "":
         comment.pop()
       if comment[0] == "":
         comment.pop(0)
       files[name].append(comment)
+      
 
     
 #pprint.pprint(files)   
 
 print " "
 print "--------------------------------------------------------"
-for line in ls_sp[1:]: # loop through lines of ls output
+for line in ls_sp[1:]:                              # loop through lines of ls output
   line_sp = line.split()
-  name = ansi_escape.sub("",line_sp[-1])            # remove ansi color codes before checking for dictionary key
+  if line[0] == "l":
+    name = ansi_escape.sub("",line_sp[-3])    
+  else:  
+    name = ansi_escape.sub("",line_sp[-1])            # remove ansi color codes before checking for dictionary key
   if line_sp[-1][0:4] == "\x1b[0m":
     line_sp[-1] = line_sp[-1].replace("\x1b[0m","") # some names won't print with underline because they begin with \x1b[0m
   line_sp[-1] = UNDERLINE + line_sp[-1] + ENDC      # add underline
--- sfd.py
+++ sfd.py
@@ -6,6 +6,8 @@ import os
 import re
 
 editor = "nano"
+#method = "fattr"
+method = "file"
 
 # check argument list
 n = len(sys.argv)
@@ -26,7 +28,7 @@ full_name = pwd+"/"+file_name
 if not os.path.exists(full_name):
   print "sfd: file does not exist"
   raise SystemExit(0)
-
+  
 # check if owner has write permissions
 #   - This will need to be considered more carefully if used when different owners/groups are involved
 cmd = "ls -ld " + full_name  
@@ -43,59 +45,89 @@ if ls_output[2] != "w":
     print "sfd: owner does not have write permissions"  
     print ls_output
     raise SystemExit(0)
+  
+if method == "fattr":
 
-# check if object already has extended attributes  
-cmd = "getfattr -d --absolute-names " + full_name
-existing_attr = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,executable='/bin/bash').communicate()[0]
+  # check if object already has extended attributes  
+  cmd = "getfattr -d --absolute-names " + full_name
+  existing_attr = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,executable='/bin/bash').communicate()[0]
 
-# open dummy file for extended attribute input (overwrite if already exists)
-f = open(".descrip","w")
+  # open dummy file for extended attribute input (overwrite if already exists)
+  f = open(".descrip","w")
 
-# write any existing object extended attibutes to file
-if existing_attr != "":
-  file_info = filter(None,existing_attr.split("\n"))  #split output lines and get rid of empty strings
-  for i in range(1,len(file_info)):  # loop through list of attribute values (begins on second line)
+  # write any existing object extended attibutes to file
+  if existing_attr != "":
+    file_info = filter(None,existing_attr.split("\n"))  #split output lines and get rid of empty strings
+    for i in range(1,len(file_info)):  # loop through list of attribute values (begins on second line)
                                      # just in case other attrubutes are set, this program only uses the user.comment attribute
-    ind = file_info[i].find("user.comment=")                                 
-    if ind >= 0:                                     
-      ind =ind+len("user.comment=")                           # find where comment begins
-      descrip = file_info[i].replace(file_info[i][:ind+1],"") # remove the name of the extended attrubute
-      comment = descrip.replace('"','').split(r"\012")        # split lines and remove quotes
+      ind = file_info[i].find("user.comment=")                                 
+      if ind >= 0:                                     
+        ind =ind+len("user.comment=")                           # find where comment begins
+        descrip = file_info[i].replace(file_info[i][:ind+1],"") # remove the name of the extended attrubute
+        comment = descrip.replace('"','').split(r"\012")        # split lines and remove quotes
  
-      for line in comment:    # write to file
-        f.write("%s\n" % line)
+        for line in comment:    # write to file
+          f.write("%s\n" % line)
       
-f.write("\n")
-f.write("\n")
-f.write("\n")
-f.write("# Enter comment for: " + file_name + "\n")
-f.write("# Full path: " + full_name + "\n")      
+  f.write("\n")
+  f.write("\n")
+  f.write("\n")
+  f.write("# Enter comment for: " + file_name + "\n")
+  f.write("# Full path: " + full_name + "\n")      
     
-f.close()    
+  f.close()    
 
-# Allow user to input comments and read them in 
-subprocess.call([editor,".descrip"])
-f = open(".descrip")
-cmnt = f.read().strip()
-f.close()
+  # Allow user to input comments and read them in 
+  subprocess.call([editor,".descrip"])
+  f = open(".descrip")
+  cmnt = f.read().strip()
+  f.close()
 
-# Process input comments
-cmnt_sp = cmnt.split("\n")
-cmnt_ls = []
-for line in cmnt_sp:
-  if line and line[0] != "#": # leave out commented lines...blank lines are also skipped
-    cmnt_ls.append(line)    
-cmnt = "\n".join(cmnt_ls)
+  # Process input comments
+  cmnt_sp = cmnt.split("\n")
+  cmnt_ls = []
+  for line in cmnt_sp:
+    if line and line[0] != "#": # leave out commented lines...blank lines are also skipped
+      cmnt_ls.append(line)    
+  cmnt = "\n".join(cmnt_ls)
 
-if cmnt == "":
-  print "sfd: comment is empty, exiting..."
-  raise SystemExit(0)
+  if cmnt == "":
+    print "sfd: comment is empty, exiting..."
+    raise SystemExit(0)
 
 
-# Add the inputed comment as an extended attribute
-cmd = 'setfattr -n user.comment -v "' + cmnt + '" ' + full_name
-subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,executable='/bin/bash').communicate()[0]
+  # Add the inputed comment as an extended attribute
+  cmd = 'setfattr -n user.comment -v "' + cmnt + '" ' + full_name
+  subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,executable='/bin/bash').communicate()[0]
 
+elif method == "file":
+  
+  # Create description file name  
+  if os.path.islink(full_name):
+    path = os.path.realpath(full_name)
+    file_name = path.split("/")[-1]
+    descrip_fname =  path + "/." + file_name
+  else:
+    descrip_fname = pwd + "/." + file_name    
+    
+  if os.path.isfile(full_name):
+    extension = "-file.descrip"
+  elif os.path.isdir(full_name):
+    extension = "-dir.descrip"
+  else:
+    print "sfd: file type error..."
+    raise SystemExit(0) 
+  descrip_fname = descrip_fname + extension
+  
+  # Edit the description file
+  if os.path.exists(descrip_fname):    
+    subprocess.call(["chmod","+w",descrip_fname])
+  subprocess.call([editor,descrip_fname])
+  subprocess.call(["chmod","-w",descrip_fname])    
+  
+  
+  
+  
 # remove the write permissions if they were added
 if prem_mod == 1:
   cmd = "chmod u-w " + full_name        
